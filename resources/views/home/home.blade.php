6a933460f5e022592c6bb0e654b43fe8ebfa0169
Chief Technical Officer <kaprinkey1@cougars.ccis.edu>
2025-06-18 22:50:18 -0500

    Update home.blade.php

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -117,11 +117,6 @@
 </section><section class="py-10 bg-black">
     <div class="px-10 mx-auto max-w-7xl">
         <div class="flex flex-col items-center md:flex-row md:justify-between">
-            <a href="#_" class="flex items-center justify-center w-10 h-10 mr-3 rounded-lg mb-7 bg-gradient-to-br from-purple-500 via-indigo-600 to-blue-500 rounded-xxl">
-                <svg class="w-5 h-5 text-white fill-current" viewBox="0 0 39 15" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.252 1.152C4.184 2.526.454 6.918.061 14.329c1.963-4.049 4.798-5.975 8.503-5.778 2.115.112 3.84 1.295 5.75 2.603 3.11 2.133 6.712 4.601 13.281 3.324 7.068-1.374 10.798-5.766 11.191-13.177-1.963 4.049-4.798 5.975-8.503 5.779-2.115-.113-3.84-1.296-5.75-2.604-3.11-2.133-6.712-4.601-13.281-3.324z" fill-rule="evenodd"></path>
-                </svg>
-            </a>
 
             <div class="flex flex-row justify-center mb-4 -ml-4 -mr-4"> <a href="https://facebook.com/kprinkey" class="p-4 text-zinc-700 hover:text-zinc-400"> <svg class="fill-current" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6.023 16L6 9H3V6h3V4c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V6H13l-1 3H9.28v7H6.023z"></path>
