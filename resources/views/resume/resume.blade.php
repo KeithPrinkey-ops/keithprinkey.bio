6b89f6c20fccb71d3e509ab388fee24ee9fd2773
keithprinkey-ops <kaprinkey1@cougars.ccis.edu>
2025-06-21 22:05:59 -0500

    added a blogging system, custom written.

--- resources/views/resume/resume.blade.php
+++ resources/views/resume/resume.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-10 mx-auto max-w-7xl md:px-16">
         <div class="bg-white shadow-sm sm:rounded-lg p-6">
-            <iframe src="{{ asset('Keith_Prinkey_Resume.pdf') }}" class="w-full h-screen" style="height: 80vh;"></iframe>
+            <iframe src="{{ asset('KeithPrinkeyResume.pdf') }}" class="w-full h-screen" style="height: 80vh;"></iframe>
 
             <div class="mt-5 flex justify-center">
                 <x-button>
